5d624823a3cf0268c23bd6e3b93a23a73b293c14
Altaskur <105789412+altaskur@users.noreply.github.com>
2022-07-15 04:43:10 +0200

    Limpieza de código nocturna offline

--- src/buscaminas.js
+++ src/buscaminas.js
@@ -1,6 +1,6 @@
 // Datos del tablero
 row = 18;
-cell = 18
+cell = 18;
 
 // Datos del juego
 const mines = Math.floor((row * cell) / 6);
@@ -26,16 +26,14 @@ for (let rows = 0; rows <= row; rows++) {
 
     for (let cells = 0; cells <= cell; cells++) {
         let td = document.createElement('td');
-        
+
         if (cells == 0) {
             td.textContent = rows;
         } else {
-            td.style.cursor = "pointer"
             td.addEventListener('click', cellState);
             td.addEventListener('mouseup', cellOptions);
             td.classList.add('js-cell-undiscovered');
         }
-
 
         tr.append(td);
     }
@@ -63,81 +61,75 @@ for (let index = 0; index < mines; index++) {
     });
     locationMines.push([mRow, mCell]);
 }
-// // console.log("localización de minas: ", locationMines);
-
 
 // Función para calcular el estado de las celdas
 
 function cellState(event) {
-    // Una vez que se aga clic ya nunca se podrá volver a clicar.
-    event.target.removeEventListener('click', cellState);
-    // // console.log(locationMines);
 
-    // Comprobamos si había una celda pulsada anteriormente
-    if (oldCell != null) {
-        oldCell.style.border = "inherit";
-    }
-    // Variables de la celda
-    let nearbyMines = 0;
+    // Comprobamos el estado de la celda si esta con opciones o no
     let cell = event.target;
-    oldCell = event.target;
-
-    // Asignamos un color al borde de la celda seleccionada
-    cell.style.border = "1px solid green";
-
-    // Obtenemos la fila actual, previa y siguiente
-    let parent = event.target.parentNode;
-    // let siblingsPrevius = parent.previousElementSibling;
-    // let siblingsNext = parent.nextElementSibling;
-
-    // obtenemos su posición de celda respecto a su fila
-    let cIndex = event.target.cellIndex;
-    let rIndex = parent.rowIndex;
-
-    // // // console.log("siblingsPrevius: ", siblingsPrevius);
-    // // // console.log("parent: ", parent);
-    // // // console.log("siblingsNext: ",siblingsNext);
-
-    // // // console.log("cIndex: ", cIndex);
-    // // // console.log("rIndex: ", rIndex);
-
-    let isMine = detectMine(rIndex, cIndex);
-    // // console.log("Datos de la celda: ", rIndex, cIndex, isMine);
-
-
-    if (isMine === false) {
-        cell.style.backgroundColor = "rgb(250, 250, 250)";
-
-        nearbyMines = numberOfMinesArrounCell(rIndex, cIndex, nearbyMines);
-
-        // // console.log("Total de minas: ", nearbyMines);
-
-        // Si existen minas al rededor de la celda
-        // mostramos el número de minas en rojo
-        // en caso contrario mostramos 0 en azul.
-        if (nearbyMines > 0) {
-            cell.textContent = nearbyMines;
-
-            cell.classList.remove('js-cell-undiscovered');
-            cell.classList.add('js-cell-discovered');
-        } else {
-            // // console.log("No hay minas alrededor");
-            // Evaluamos las celdas adyacentes
-
-            numberOfMinesAdjacent(rIndex, cIndex, nearbyMines)
+    let cellOptions = false;
+    cell.classList.forEach(cellState => {
+        if (cellState == "js-cell-options") {
+            cellOptions = true;
         }
+    });
 
-    } else if (isMine === true) {
-        // // console.log("¡Has perdido!");
-        cell.style.backgroundColor = "red";
-        cell.style.border = "inherit";
-        cell.textContent = "💣";
-        console.warn("¡Has perdido!");
-        alert("Has perdido");
+    if (!cellOptions) {
+
+        // Comprobamos si había una celda pulsada anteriormente
+        if (oldCell != null) {
+            oldCell.classList.remove("js-cell-selected");
+        }
+        // Una vez que se aga clic ya nunca se podrá volver a hacer clic.
+
+        cell.removeEventListener('click', cellState);
+
+        // Variables de la celda
+        let nearbyMines = 0;
+
+        oldCell = event.target;
+
+        // Asignamos un color al borde de la celda seleccionada
+        cell.classList.add("js-cell-selected");
+
+        // Obtenemos la fila actual, previa y siguiente
+        let parent = event.target.parentNode;
+        // let siblingsPrevius = parent.previousElementSibling;
+        // let siblingsNext = parent.nextElementSibling;
+
+        // obtenemos su posición de celda respecto a su fila
+        let cIndex = event.target.cellIndex;
+        let rIndex = parent.rowIndex;
+
+        let isMine = detectMine(rIndex, cIndex);
+
+        if (isMine === false) {
+            cell.classList.remove("js-cell-undiscovered");
+            cell.classList.add("js-cell-discovered");
+            nearbyMines = numberOfMinesAroundCell(rIndex, cIndex, nearbyMines);
+
+            // Si existen minas al rededor de la celda
+            // mostramos el número de minas en rojo
+            // en caso contrario mostramos 0 en azul.
+            if (nearbyMines > 0) {
+                cell.textContent = nearbyMines;
+
+            } else {
+
+                // Evaluamos las celdas adyacentes
+                numberOfMinesAdjacent(rIndex, cIndex, nearbyMines)
+            }
+
+        } else if (isMine === true) {
+            cell.classList.add("js-cell-mine");
+
+            console.warn("¡Has perdido!");
+            alert("Has perdido");
+        }
+        //  gym + paseo ? cansado++ : feliz;
     }
-    //  gym + paseo ? cansado++ : feliz;
 }
-
 
 function detectMine(row, cell) {
     let isMine = false;
@@ -145,31 +137,25 @@ function detectMine(row, cell) {
     locationMines.forEach(function (mine) {
         if (mine[0] === row && mine[1] === cell) {
             isMine = true;
-            // // console.log("Mina localizada en: ", mine);
         }
     });
     return isMine;
 }
 
-function numberOfMinesArrounCell(rIndex, cIndex, nearbyMines) {
+function numberOfMinesAroundCell(rIndex, cIndex, nearbyMines) {
     console.log("Ejecutando función numberOfMinesArrounCell");
     // Comprobamos la fila anterior, actual y siguiente.
     let checkRows = [rIndex - 1, rIndex, rIndex + 1];
 
     for (checkRow in checkRows) {
 
-
         let actualRow = checkRows[checkRow]; // fila actual
         let actualCell = cIndex - 1; // celda actual
-
-        // console.log("Evaluando fila: ", actualRow);
 
         if (actualRow >= 0 && actualRow < row) {
             for (let index = 0; index <= 2; index++) {
 
                 if (actualCell >= 0 && actualCell < cell) {
-
-                    // console.log("Evaluando la celda: ", actualRow, actualCell);
 
                     if (!(actualRow == rIndex && actualCell == cIndex)) {
                         detectMine(actualRow, actualCell) ? nearbyMines++ : null;
@@ -179,10 +165,6 @@ function numberOfMinesArrounCell(rIndex, cIndex, nearbyMines) {
 
             }
         }
-
-
-
-        // // console.log("Minas alrededor: ", nearbyMines);
 
     }
     return nearbyMines;
@@ -194,26 +176,6 @@ function numberOfMinesAdjacent(rIndex, cIndex) {
 
     let checkCells = [cIndex - 1, cIndex, cIndex + 1];
 
-    // let superiorRow = checkRows[0];
-    // let actualRow = checkRows[1];
-    // let inferiorRow = checkRows[2];
-
-    // let leftCell = checkCells[0];
-    // let actualCell = checkCells[1];
-    // let rightCell = checkCells[2];
-
-    // // Comprobamos las celda superior
-    // let superiorCellMines = numberOfMinesArrounCell(superiorRow, actualCell, 0);
-    // // Si no hay minas al rededor de la celda subyacente 
-    // // Despejamos la celda
-
-    // // Comprobamos la celda izquierda y derecha
-    // let leftCellMines = numberOfMinesArrounCell(actualRow, leftCell, 0);
-    // let rightCellMines = numberOfMinesArrounCell(actualRow, rightCell, 0);
-
-    // // Comprobamos la celda inferior
-    // let inferiorCellMines = numberOfMinesArrounCell(inferiorRow, actualCell, 0);
-
     let minesDetect = {
         superiorRow: null,
         inferiorRow: null,
@@ -224,24 +186,19 @@ function numberOfMinesAdjacent(rIndex, cIndex) {
     // recorremos las filas
     for (let rIndex = 0; rIndex < 3; rIndex++) {
 
-
         if (checkRows[rIndex] >= 0 && checkRows[rIndex] <= row) {
 
             // recorremos las celdas
             for (let cIndex = 0; cIndex < 3; cIndex++) {
 
-
                 if (checkCells[cIndex] > 0 && checkCells[cIndex] <= cell) {
-
 
                     let actualRow = checkRows[rIndex];
                     let actualCell = checkCells[cIndex];
 
-                    let results = numberOfMinesArrounCell(actualRow, actualCell, 0);
+                    let results = numberOfMinesAroundCell(actualRow, actualCell, 0);
 
                     // Evaluando las celdas superior, inferior, izquierda y derecha
-
-
                     // Comprobamos celdas izquierda y derecha
 
                     console.log("Evaluando celda: ", actualRow, actualCell);
@@ -262,17 +219,13 @@ function numberOfMinesAdjacent(rIndex, cIndex) {
     // Capturamos las filas y celdas del tablero
     let tbody = document.querySelector("tbody");
     let rows = tbody.querySelectorAll("tr");
-    // // // console.log("rows: ", rows);
 
     cellsEmpty = [];
 
     if (minesDetect.superiorRow != null) {
         boardSuperiorCell = rows[minesDetect.superiorRow[1]].querySelectorAll("td")[minesDetect.superiorRow[2]];
-        boardSuperiorCell.style.backgroundColor = "rgb(252, 252, 252)";
-        boardSuperiorCell.style.fontWeight = "bold";
-        boardSuperiorCell.style.color = "red";
-        boardSuperiorCell.style.border = "none";
-        boardSuperiorCell.style.cursor = "initial";
+        boardSuperiorCell.classList.remove("js-cell-undiscovered");
+        boardSuperiorCell.classList.add("js-cell-discovered");
 
         boardSuperiorCell.removeEventListener("click", cellState);
 
@@ -285,14 +238,10 @@ function numberOfMinesAdjacent(rIndex, cIndex) {
     }
     if (minesDetect.inferiorRow != null) {
         boardInferiorCell = rows[minesDetect.inferiorRow[1]].querySelectorAll("td")[minesDetect.inferiorRow[2]];
-        boardInferiorCell.style.backgroundColor = "rgb(252, 252, 252)";
-        boardInferiorCell.style.fontWeight = "bold";
-        boardInferiorCell.style.color = "red";
-        boardInferiorCell.style.border = "none";
-        boardInferiorCell.style.cursor = "initial";
+        boardInferiorCell.classList.remove("js-cell-undiscovered");
+        boardInferiorCell.classList.add("js-cell-discovered");
 
         boardInferiorCell.removeEventListener("click", cellState);
-
 
         if (minesDetect.inferiorRow[0] > 0) {
             boardInferiorCell.textContent = minesDetect.inferiorRow[0];
@@ -302,11 +251,8 @@ function numberOfMinesAdjacent(rIndex, cIndex) {
     }
     if (minesDetect.leftCell != null) {
         boardSelectedLeftCell = rows[minesDetect.leftCell[1]].querySelectorAll("td")[minesDetect.leftCell[2]];
-        boardSelectedLeftCell.style.backgroundColor = "rgb(252, 252, 252)";
-        boardSelectedLeftCell.style.fontWeight = "bold";
-        boardSelectedLeftCell.style.color = "red";
-        boardSelectedLeftCell.style.border = "none";
-        boardSelectedLeftCell.style.cursor = "initial";
+        boardSelectedLeftCell.classList.remove("js-cell-undiscovered");
+        boardSelectedLeftCell.classList.add("js-cell-discovered");
 
         boardSelectedLeftCell.removeEventListener("click", cellState);
 
@@ -319,11 +265,8 @@ function numberOfMinesAdjacent(rIndex, cIndex) {
     }
     if (minesDetect.rightCell != null) {
         boardSelectedRightCell = rows[minesDetect.rightCell[1]].querySelectorAll("td")[minesDetect.rightCell[2]];
-        boardSelectedRightCell.style.backgroundColor = "rgb(252, 252, 252)";
-        boardSelectedRightCell.style.fontWeight = "bold";
-        boardSelectedRightCell.style.color = "red";
-        boardSelectedRightCell.style.border = "none";
-        boardSelectedRightCell.style.cursor = "initial";
+        boardSelectedRightCell.classList.remove("js-cell-undiscovered");
+        boardSelectedRightCell.classList.add("js-cell-discovered");
 
         boardSelectedRightCell.removeEventListener("click", cellState);
 
@@ -343,25 +286,24 @@ function numberOfMinesAdjacent(rIndex, cIndex) {
 
 function cellOptions(event) {
 
-    console.log(event)
     let cell = event.target;
     cellContent = cell.textContent;
-    
+
     // comprobamos si es el botón derecho
     if (event.button == 2) {
 
-        if(cellContent == "" && cell.classList[0] == "td.js-cell-undiscovered" )  {
+        if (cellContent == "" && cell.classList[0] == "js-cell-undiscovered") {
+            cell.classList.add("js-cell-options");
             cell.textContent = "🚩";
-            
+
         } else if (cellContent == "🚩") {
             cell.textContent = "❓";
 
         } else if (cellContent == "❓") {
             cell.textContent = "";
-            
+            cell.classList.remove("js-cell-options");
         }
     }
-
 }
 
 /*
